20b8cb91d111e43f69053cce036cea2c8e97d346
shreyansh-web <shreyansh487@gmail.com>
2025-01-23 02:24:28 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { Provider } from "react-redux";
-import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import { store } from "./store/store";
 import Board from "./components/Board";
 
-const theme = createTheme();
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
         <Board />
-      </ThemeProvider>
     </Provider>
   );
 }
